d88bca527fbfa6d9c9b022f57ef110102825ea5b
Maharshi <maharshid32@gmail.com>
2026-06-20 03:25:02 +0530

    Refactor VPC module variable references for consistency
    Signed-off-by: Maharshi <maharshid32@gmail.com>

--- infra/modules/vpc/maint.tf
+++ infra/modules/vpc/maint.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
   version              = "~> 5.0"
-  name                 = var.vpc.name
-  cidr                 = var.vpc.cidr
-  azs                  = var.vpc.azs
-  private_subnets      = var.vpc.private_subnets
-  public_subnets       = var.vpc.public_subnets
-  enable_nat_gateway   = var.vpc.enable_nat_gateway
-  enable_dns_hostnames = var.vpc.enable_dns_hostnames
+  name                 = var.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.vpc_azs
+  private_subnets      = var.vpc_private_subnets
+  public_subnets       = var.vpc_public_subnets
+  enable_nat_gateway   = var.vpc_enable_nat_gateway
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
