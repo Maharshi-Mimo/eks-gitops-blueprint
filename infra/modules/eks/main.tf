317f92b8e601aaa7629a6d38452c4fdc22266130
Maharshi <maharshid32@gmail.com>
2026-06-27 23:57:51 +0530

    fix (#34): Correct addon keys format in EKS module
    Signed-off-by: Maharshi <maharshid32@gmail.com>

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -18,9 +18,9 @@ module "eks" {
     }
   }
   cluster_addons = {
-    vpc_cni            = { most_recent = true }
+    vpc-cni            = { most_recent = true }
     coredns            = { most_recent = true }
-    kube_proxy         = { most_recent = true }
-    aws_ebs_csi_driver = { most_recent = true }
+    kube-proxy         = { most_recent = true }
+    aws-ebs-csi-driver = { most_recent = true }
   }
 }
